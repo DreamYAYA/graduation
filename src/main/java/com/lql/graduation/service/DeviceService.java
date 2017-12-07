2f4f6f96c3f9768859b5cd4059ef22418c52e0e1
liquanli <836957413@qq.com>
2017-12-07 08:30:11 +0800

    update

--- src/main/java/com/lql/graduation/service/DeviceService.java
+++ src/main/java/com/lql/graduation/service/DeviceService.java
@@ -2,8 +2,12 @@ package com.lql.graduation.service;
 
 import com.lql.graduation.pojo.Device;
 
+import java.util.List;
+
 public interface DeviceService {
 
 
     Integer addDevice(Device device,String Uid);
+
+    List<Device> deviceList();
 }
