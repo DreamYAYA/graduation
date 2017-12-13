1064d5c9ef75344e5b5757ef0e7eda4b00925ef4
liquanli <836957413@qq.com>
2017-12-13 15:04:59 +0800

    添加报警提示列表

--- src/main/java/com/lql/graduation/listeren/ReciverMessageListeren.java
+++ src/main/java/com/lql/graduation/listeren/ReciverMessageListeren.java
@@ -39,48 +39,61 @@ private DeviceInterfaceDataService deviceInterfaceDataService;
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         aliConfig config = new aliConfig();
         CloudQueue queue = config.getQue(Constant.aliDevice.QUEUE_DEFAULT_NAME);
-        while(true){
 
-            Message popMsg = queue.popMessage(10); //长轮询等待时间为10秒
-            if (popMsg != null) {
-                String rowMessage = popMsg.getMessageBodyAsString();
-                try {
-                    Map rowMap = JsonUtil.JsonToMap(rowMessage);
-                    String messagetype = (String)rowMap.get("messagetype");//获取消息类型：1.是设备的发送的消息,2，是设备上下线的消息
-                    String playload =(String)rowMap.get("payload"); //获取设备消息
-                    System.out.println(messagetype+":"+playload);
-                    if(MESSAGE_TYPE_STATUS.equals(messagetype)){
-                        //是设备的通知数据(上下线的消息)
-                        deviceService = getService(servletContextEvent,DeviceService.class);
-                        DataBean deviceData = new DataBean();
-                        deviceData = (DataBean)JsonUtil.JsonToObject(new String(Base64Utils.decodeFromString(playload),"utf-8"), DataBean.class);
-                        deviceService.updateDevice(deviceData);//更新设备的状态
+        new Thread(){
 
-                    }else if(MESSSAGE_TYPE_UPLOAD.equals(messagetype)){
-                        //是设备的发送的消息
-                        deviceInterfaceDataService = getService(servletContextEvent,DeviceInterfaceDataService.class);
-                        DeviceData deviceData = new DeviceData();
-                        deviceData = (DeviceData)JsonUtil.JsonToObject(new String(Base64Utils.decodeFromString(playload),"utf-8"), DeviceData.class);
+            @Override
+            public void run() {
+                while(true){
 
-                        DeviceInterfaceData deviceInterfaceData = new DeviceInterfaceData();
-                        deviceInterfaceData.setDataId(UidUtils.getUid());
-                        deviceInterfaceData.setDeviceInterfaceId(deviceData.getInterfaces());
-                        deviceInterfaceData.setDeviceInerfaceData(deviceData.getMessage());
+                    Message popMsg = queue.popMessage(10); //长轮询等待时间为10秒
+                    if (popMsg != null) {
+                        String rowMessage = popMsg.getMessageBodyAsString();
+                        try {
+                            Map rowMap = JsonUtil.JsonToMap(rowMessage);
+                            String messagetype = (String)rowMap.get("messagetype");//获取消息类型：1.是设备的发送的消息,2，是设备上下线的消息
+                            String playload =(String)rowMap.get("payload"); //获取设备消息
+                            System.out.println(messagetype+":"+playload);
+                            if(MESSAGE_TYPE_STATUS.equals(messagetype)){
+                                //是设备的通知数据(上下线的消息)
+                                deviceService = getService(servletContextEvent,DeviceService.class);
+                                DataBean deviceData = new DataBean();
+                                deviceData = (DataBean)JsonUtil.JsonToObject(new String(Base64Utils.decodeFromString(playload),"utf-8"), DataBean.class);
+                                deviceService.updateDevice(deviceData);//更新设备的状态
 
-                        deviceInterfaceDataService.insertInterfaceData(deviceInterfaceData);
+                            }else if(MESSSAGE_TYPE_UPLOAD.equals(messagetype)){
+                                //是设备的发送的消息
+                                deviceInterfaceDataService = getService(servletContextEvent,DeviceInterfaceDataService.class);
+                                DeviceData deviceData = new DeviceData();
+                                deviceData = (DeviceData)JsonUtil.JsonToObject(new String(Base64Utils.decodeFromString(playload),"utf-8"), DeviceData.class);
 
+                                DeviceInterfaceData deviceInterfaceData = new DeviceInterfaceData();
+                                deviceInterfaceData.setDataId(UidUtils.getUid());
+                                deviceInterfaceData.setDeviceInterfaceId(deviceData.getInterfaces());
+                                deviceInterfaceData.setDeviceInerfaceData(deviceData.getMessage());
+
+                                deviceInterfaceDataService.insertInterfaceData(deviceInterfaceData);
+
+                            }
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            System.out.println("Json解析错误");
+                        }
+                        queue.deleteMessage(popMsg.getReceiptHandle()); //从队列中删除消息
+                    } else {
+                        System.out.println("Continuing");
                     }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.out.println("Json解析错误");
+                    try {
+                        sleep(10000);
+                        System.out.println("haha");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                queue.deleteMessage(popMsg.getReceiptHandle()); //从队列中删除消息
-            } else {
-                System.out.println("Continuing");
             }
+        }.start();
 
-        }
 
     }
 
