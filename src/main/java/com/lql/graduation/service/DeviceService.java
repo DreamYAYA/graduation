163197338aca50c89072c4db0459c452f05a220c
liquanli <836957413@qq.com>
2018-02-07 13:51:35 +0800

    修改前端首页的展示使用AdminLte插件

--- src/main/java/com/lql/graduation/service/DeviceService.java
+++ src/main/java/com/lql/graduation/service/DeviceService.java
@@ -8,16 +8,16 @@ import java.util.List;
 
 public interface DeviceService {
 
-
+        //添加设备
     Integer addDevice(Device device,String Uid);
-
+    //查询设备列表
     List<Device> deviceList();
-
+        //更新设备信息
     Integer updateDevice(DataBean deviceData);
-
+    //根据ID查找指定设备
     Device findDeviceById(String deviceId);
-
+    //查询在线设备
     List<Device> onlineDeviceList(Integer status);
-
+        //发送消息至设备
     ServerResponse sendMessageToDevice(String deviceId, String controllerMsg);
 }
