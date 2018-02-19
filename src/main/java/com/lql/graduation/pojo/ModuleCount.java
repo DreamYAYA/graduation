9837983d48c462f5c702a0642bc3df541aed01ae
liquanli <836957413@qq.com>
2018-02-19 22:21:48 +0800

    首页设备，数据接口，报警接口数量统计

---
+++ src/main/java/com/lql/graduation/pojo/ModuleCount.java
@@ -0,0 +1,32 @@
+package com.lql.graduation.pojo;
+
+public class ModuleCount {
+
+    private  Integer deviceCount;
+    private Integer alertCount;
+    private Integer interfaceCount;
+
+    public Integer getDeviceCount() {
+        return deviceCount;
+    }
+
+    public void setDeviceCount(Integer deviceCount) {
+        this.deviceCount = deviceCount;
+    }
+
+    public Integer getAlertCount() {
+        return alertCount;
+    }
+
+    public void setAlertCount(Integer alertCount) {
+        this.alertCount = alertCount;
+    }
+
+    public Integer getInterfaceCount() {
+        return interfaceCount;
+    }
+
+    public void setInterfaceCount(Integer interfaceCount) {
+        this.interfaceCount = interfaceCount;
+    }
+}
